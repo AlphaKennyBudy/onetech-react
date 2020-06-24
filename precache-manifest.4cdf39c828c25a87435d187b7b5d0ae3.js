97918daa127fa91fa0a5c30186183edb51ee2da4
AlphaKennyBudy <mr.adil777@gmail.com>
2020-06-24 19:58:01 +0600

    Updates

---
+++ precache-manifest.4cdf39c828c25a87435d187b7b5d0ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151c7956bec0db24e05c2d61af26f7db",
+    "url": "/onetech-react/index.html"
+  },
+  {
+    "revision": "230c285c22444e68b2d8",
+    "url": "/onetech-react/static/css/main.a8a603d8.chunk.css"
+  },
+  {
+    "revision": "5ba38e0256a55fd43dc6",
+    "url": "/onetech-react/static/js/2.40d2873f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/onetech-react/static/js/2.40d2873f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "230c285c22444e68b2d8",
+    "url": "/onetech-react/static/js/main.5369a2d1.chunk.js"
+  },
+  {
+    "revision": "d62a3bd4a701a20bb325",
+    "url": "/onetech-react/static/js/runtime-main.9237b468.js"
+  }
+]);
