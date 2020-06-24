0ad6b0a4e2f75b0eea0bc8675e74c5f1006067a7
AlphaKennyBudy <mr.adil777@gmail.com>
2020-06-24 20:03:25 +0600

    DONE: Minor changes

--- src/components/ContactRow.js
+++ src/components/ContactRow.js
@@ -8,7 +8,7 @@ function ContactRow(props) {
       <Container fluid className="ContactRow">
           <Row>
               <Col xs={4}>
-                <Image src="https://avatars2.githubusercontent.com/u/48881444?s=460&u=a2317274ce4b7c57e3c87e604e55595d65d02a2a&v=4"  className="ProfileImage" roundedCircle/>
+                <Image src="https://picsum.photos/200"  className="ProfileImage" roundedCircle/>
               </Col>
               <Col>
                 <h1 className="ProfileName">{props.name}</h1>
