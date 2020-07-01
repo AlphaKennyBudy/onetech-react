a6033b3dd3dafde6a219110ea0aa91a16a5d475f
AlphaKennyBudy <mr.adil777@gmail.com>
2020-07-01 20:20:48 +0600

    DONE: Project component is done

--- src/pages/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ProjectPage.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-function ProjectPage() {
-  return <div className="ProjectPage"></div>;
-}
+import "./ProjectPage.css";
 
+function ProjectPage() {
+  return (
+    <div className="ProjectPage">
+      <h1 className="ProjectTitle">Task2 - Phone Book</h1>
+      <iframe
+        className="ProjectFrame"
+        src="https://onetech-react.vercel.app/"
+        title="Task2 - Phone Book"
+      ></iframe>
+      <h1 className="ProjectTitle">Task 3 - Notes</h1>
+      <iframe
+        className="ProjectFrame"
+        src="https://onetech-react.alphakennybudy.vercel.app/"
+        title="Task 3 - Notes"
+      ></iframe>
+    </div>
+  );
+}
 export default ProjectPage;
