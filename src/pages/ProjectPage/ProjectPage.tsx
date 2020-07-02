3e18116d0058f2a62d63b0fc4240c0e0b417c477
AlphaKennyBudy <mr.adil777@gmail.com>
2020-07-02 16:30:57 +0600

    DONE: new project has been added in project page

--- src/pages/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ProjectPage.tsx
@@ -8,14 +8,20 @@ function ProjectPage() {
       <h1 className="ProjectTitle">Task2 - Phone Book</h1>
       <iframe
         className="ProjectFrame"
-        src="https://onetech-react.vercel.app/"
+        src="https://onetech-react-task2.now.sh"
         title="Task2 - Phone Book"
       ></iframe>
       <h1 className="ProjectTitle">Task 3 - Notes</h1>
       <iframe
         className="ProjectFrame"
-        src="https://onetech-react-8d40d8zp7.vercel.app/"
+        src="https://onetech-react-task3.now.sh"
         title="Task 3 - Notes"
+      ></iframe>
+      <h1 className="ProjectTitle">Task 5 - Login Form</h1>
+      <iframe
+        className="ProjectFrame"
+        src="https://onetech-react-task5.now.sh"
+        title="Task 5 - Login Form"
       ></iframe>
     </div>
   );
