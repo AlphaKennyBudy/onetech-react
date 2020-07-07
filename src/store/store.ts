d3b5ff512435dff5e159857300a98493125caef7
AlphaKennyBudy <mr.adil777@gmail.com>
2020-07-07 17:20:31 +0600

    DONE: Devtools removed

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,8 @@
 import { createStore } from "redux";
 import counterReducer from "../reducers/counterReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
-let store = createStore(counterReducer, composeWithDevTools());
+// let store = createStore(counterReducer, composeWithDevTools());
+let store = createStore(counterReducer);
 
 export default store;
